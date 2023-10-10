0a4fd89eceb6cdb29825c7fffcc007c0c29b831a
Mikko Savolainen <mikko.savolainen@gofore.com>
2023-10-10 08:53:01 +0300

    build: temporarily remove custom translation from fileupload to fix build

--- frontend/src/employee-frontend/components/placement-tool/PlacementToolPage.tsx
+++ frontend/src/employee-frontend/components/placement-tool/PlacementToolPage.tsx
@@ -25,7 +25,7 @@ export default React.memo(function PlacementToolPage() {
           onUpload={uploadPlacementFile}
           onDelete={deletePlacementFile}
           getDownloadUrl={() => ''}
-          i18n={i18n.placementTool.fileUpload}
+          // i18n={i18n.placementTool.fileUpload}
           accept={['text/csv']}
         />
       </ContentArea>
